ee286c7baf596606dbf99c488b73ffe4a081f5ab
James Oliver <joliver@rancher.com>
2016-12-08 17:59:01 -0700

    nfsv3 support

--- package/common/common.sh
+++ package/common/common.sh
@@ -72,14 +72,23 @@ print_device()
     echo -n "$@" | jq -R -c -s '{"status": "Success", "device": .}'
 }
 
-print_not_supported()
-{
+print_not_supported() {
     echo -n "$@" | jq -R -c -s '{"status": "Not supported", "message": .}'
+}
+
+exit_not_supported() {
+    print_not_supported "$@"
+    exit 0
 }
 
 print_success()
 {
     echo -n "$@" | jq -R -c -s '{"status": "Success", "message": .}'
+}
+
+exit_success() {
+    print_success "$@"
+    exit 0
 }
 
 print_error()
@@ -108,4 +117,60 @@ unset_aws_credentials_env() {
 get_host_process_pid() {
     PARENT_PID=$(ps --no-header --pid $$ -o ppid)
     TARGET_PID=$(ps --no-header --pid ${PARENT_PID} -o ppid)
+}
+
+init_nfs_client_service() {
+    # using host network context to start rpcbind and rpc.statd inside the container process.
+    # this requires mapping host pid namespace to this containers when container starts.
+    # here parent is the storage --driver rancher-nfs process, the script process is launched
+    # on demand of each create/delete etc calls, so host process pid is 2 hops away
+    get_host_process_pid
+    if [ ! "$(pidof rpcbind)" ] && [ ! "$(pidof rpc.statd)" ]; then
+        nsenter -t $TARGET_PID -n rpcbind >& /dev/null
+        nsenter -t $TARGET_PID -n rpc.statd >& /dev/null
+    fi
+}
+
+must_exist() {
+    local name="$1"
+    local var="$2"
+    if [ -z "$var" ]; then
+        print_error "Failed: No variable $name found"
+    fi
+}
+
+mount_nfs() {
+    local server=$1
+    local dir=$2
+    local dest=$3
+    local opts=$4
+    local error
+
+    get_host_process_pid
+
+    MOUNT_CMD="mount -t nfs"
+    if [ ! -z "$opts" ]; then
+        MOUNT_CMD="$MOUNT_CMD -o $opts"
+    fi
+
+    if [ "$(ismounted $dest)" == 0 ]; then
+        mkdir -p $dest
+
+        error=`nsenter -t ${TARGET_PID} -n -m $MOUNT_CMD "$server":"$dir" "$dest" 2>&1`
+        if [ $? -ne 0 ]; then
+            print_error "Mount failed: $error"
+        fi
+    fi
+}
+
+unmount_dir() {
+    local dest=$1
+    if [ "$(ismounted $dest)" == 1 ]; then
+        error=`umount $dest 2>&1`
+        if [ $? -ne 0 ]; then
+            print_error "Umount failed: $error"
+        elif [ ! "$(ls -A $dest)" ]; then
+            rmdir $dest
+        fi
+    fi
 }
